f600d632f0f7d3f0e339ca23ca3617eeb9c9fa03
Lucio Mitsuru Seki <lucioseki@gmail.com>
2012-12-05 12:43:15 -0200

    o teta eh convertido em radianos

--- e2.m
+++ e2.m
@@ -4,12 +4,12 @@
 # C1 = constante
 # w = frequencia angular = (2*pi / T0) = (2*pi * f0)
 # t = variavel de tempo
-# teta = angulo de fase (deslocamento horizontal do sinal)
+# teta = angulo de fase em graus (deslocamento horizontal do sinal)
 #
 # uso: e2(A0, C1, w, t, teta)
 # exemplo: plot(e2(3, 3, 2, [0:0.1:(2*pi)], 30))
 function s = e2(A0, C1, w, t, teta)
  
-c = cos(w*t+teta);
+c = cos(w*t+(teta * (pi/180)))
  
 s = A0 + C1 * c;
