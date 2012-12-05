1033b42011aa8a6e06e99b338a1f8beddab9d73e
Lucio Mitsuru Seki <lucioseki@gmail.com>
2012-12-05 12:29:58 -0200

    e2 pronto.

---
+++ e2.m
@@ -0,0 +1,15 @@
+# s(t) = A0 + C1 * cos(w*t + teta)
+#
+# A0 = altura media do sinal em relacao as abcissas
+# C1 = constante
+# w = frequencia angular = (2*pi / T0) = (2*pi * f0)
+# t = variavel de tempo
+# teta = angulo de fase (deslocamento horizontal do sinal)
+#
+# uso: e2(A0, C1, w, t, teta)
+# exemplo: plot(e2(3, 3, 2, [0:0.1:(2*pi)], 30))
+function s = e2(A0, C1, w, t, teta)
+ 
+c = cos(w*t+teta);
+ 
+s = A0 + C1 * c;
