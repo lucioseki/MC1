99ed8d2d6a17b7c1ee4418d9ee8c0b8c836112c8
Lucio Mitsuru Seki <lucioseki@gmail.com>
2012-12-05 15:35:20 -0200

    corrigi o nome da funcao

--- e2c.m
+++ e2c.m
@@ -3,6 +3,6 @@
 # uso: e2c(t)
 # exemplo: plot(e2c([0:0.1:(2*pi)]))
 
-function s = e2b(t)
+function s = e2c(t)
   s = sin(deg2rad(t) + (1/3) * sin(deg2rad(3*t))) + (1/5) * sin(deg2rad(5*t));
 endfunction
