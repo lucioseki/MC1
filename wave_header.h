e55a28cf6fe30c6b82fc89cb70d3b1192772a122
Lucio Mitsuru Seki <lucioseki@gmail.com>
2012-12-06 10:27:36 -0200

    faltou mostrar Subchunk2Size

--- wave_header.h
+++ wave_header.h
@@ -18,6 +18,7 @@ struct HeaderType{ /* cabecalho padrao de um arquivo WAVE */
 
 typedef struct HeaderType HeaderType;
 void print_header(HeaderType*);
+int numSamples(HeaderType*);
 /* O formato de arquivo WAVE e um subconjunto da especificacao RIFF da Microsoft
  * para o armazenamento de arquivos multimidia.
  */
